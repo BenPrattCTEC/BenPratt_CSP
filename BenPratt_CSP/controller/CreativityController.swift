d97c7fcec0c8eecde26239b0f01e03f4162ab742
Bcpratt2000 <bcpratt2000@gmail.com>
2017-10-26 12:05:51 -0600

    wrote in class framework

---
+++ BenPratt_CSP/controller/CreativityController.swift
@@ -0,0 +1,24 @@
+//
+//  CreativityController.swift
+//  BenPratt_CSP
+//
+//  Created by Pratt, Benjamin on 10/26/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class CreativityController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
