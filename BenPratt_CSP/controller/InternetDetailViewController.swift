e53d94c414a049d6f7cdb06febac38110c6cc3f2
Bcpratt2000 <bcpratt2000@gmail.com>
2017-12-01 13:42:36 -0700

    added more code, i dont know what it does because i spent the entire time typing instead of listening, BUT IT IS THERE

--- BenPratt_CSP/controller/InternetDetailViewController.swift
+++ BenPratt_CSP/controller/InternetDetailViewController.swift
@@ -13,12 +13,47 @@ public class InternetDetailViewController: UIViewController{
     @IBOutlet weak var textView: UILabel!
     @IBOutlet weak var webViewer: WKWebView!
     
-    override public func viewDidLoad(){
-        super.viewDidLoad()
-        setup()
+    var detailAddress: String?{
+        didSet{
+            configureDetailView()
+        }
     }
     
-    private func setup(){
+    var detailText: String?{
+        didSet{
+            configureDetailView()
+        }
+    }
+    
+    override public func viewDidLoad(){
+        super.viewDidLoad()
+        configureDetailView()
+    }
+    
+    private func configureDetailView(){
+        if detailAddress != nil{
+            if let currentWebView = webViewer{
+                let currentURL = URL(string: detailAddress!)
+                let currentWebRequest = URLRequest(url: currentURL!)
+                currentWebView.load(currentWebRequest)
+            }
+        }
+        else{
+            if let currentWebView = webViewer{
+                let currentURL = URL(string: "https://cnn.com")
+                currentWebView.load(URLRequest(url: currentURL!))
+            }
+        }
         
+        if detailText != nil{
+            if let currentText = textView{
+                currentText.text = detailText
+            }
+        }
+        else{
+            if let currentText = textView{
+                currentText.text = "Ben's CSP App Internet Screen"
+            }
+        }
     }
 }
--- BenPratt_CSP/controller/InternetMasterViewController.swift
+++ BenPratt_CSP/controller/InternetMasterViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class InternetMasterViewController: UITableViewController {
     
-    private var detailViewController: InternetDetailViewController
+    private var detailViewController: InternetDetailViewController?
+    private lazy var addresses: [String] = []
     
     private (set) lazy var internetTopics: [String] = {
         return ["Definitions","CSP","Canyons","Twitter","Swift Guide"]
@@ -30,12 +31,47 @@ class InternetMasterViewController: UITableViewController {
         return internetTopics.count
     }
     
-    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> Int{
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
         let currentText = internetTopics[indexPath.row]
         cell.textLabel!.text = currentText
         
         return cell
+    }
+    
+    override public func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if segue.identifier! == "showDetail"{
+            if let indexPath = self.tableView.indexPathForSelectedRow{
+                let urlString = addresses[indexPath.row]
+                let pageText: String
+                
+                if indexPath.row == 0{
+                    //TODO: Replace with actual definitions
+                    pageText = "ALLTTHEDEFINITIONS"
+                }
+                else{
+                    pageText = internetTopics[indexPath.row]
+                }
+                
+                let controller = segue.destination as! InternetDetailViewController
+                controller.detailAddress = urlString
+                controller.detailText = pageText
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+    
+    private func setup(){
+        //TODO: add actual addresses
+        addresses = [
+            "https://www.google.com"
+        ]
+        
+        if let splitView = splitViewController{
+            let currentControllers = splitView.viewControllers
+            detailViewController = currentControllers[0] as? InternetDetailViewController
+        }
     }
 
     override func didReceiveMemoryWarning() {
